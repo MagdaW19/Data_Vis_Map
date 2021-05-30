f77bcf428c63c90d4a8d9aa4ebdd2c71ad7da62b
Magda Wójcicka <magda.wojcicka.19.09@gmail.com>
2021-05-30 19:08:38 +0200

    Fixed setup.py - specified numpy version

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0.0',
     packages=find_packages(include=['data_vis', 'data_vis.*']),
     author='Magda Wójcicka',
-    install_requires=['folium>=0.12.1', 'pandas>=1.2.3']
+    install_requires=['folium>=0.12.1', 'pandas>=1.2.3', 'numpy>=1.16.5']
 )
